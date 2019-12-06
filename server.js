ee93920f035127445555fb61bb2d152e198d93ac
ruthtech <leeruths@gmail.com>
2019-12-06 13:55:18 -0500

    Heroku change: make / serve React index.html instead of Express /

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.listen(port, (req, res) => {  
+//   console.log( `server listening on port: ${port}`);
+// })
+
+app.use(express.static(path.join(__dirname, 'client/public')));
+
+// if(process.env.NODE_ENV === 'production') {  
+//   app.use(express.static(path.join(__dirname, 'client/public'))); 
+// } else {
+  // app.get('*', (req, res) => {    
+  //   res.sendfile(path.join(__dirname = 'client/public/index.html'));  
+  // })
+// }
+
+app.get('/', (req, res) => {  
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 // create a GET route
-app.get(['/', '/express_backend'], (req, res) => {
+app.get('/express_backend', (req, res) => {
   axios.get("https://api.adviceslip.com/advice")
   .then( 
     //res => message = res.data.slip.advice
