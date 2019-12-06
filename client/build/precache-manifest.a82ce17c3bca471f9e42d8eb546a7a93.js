9b03c045e8166f745f86bcd7093229d509c9904b
ruthtech <leeruths@gmail.com>
2019-12-06 14:57:09 -0500

    changed reacts gitignore, removed build

---
+++ client/build/precache-manifest.a82ce17c3bca471f9e42d8eb546a7a93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5361c20e33ec565cd0a7e9aa80c49cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03f8f8fd96bd7d8861ab",
+    "url": "/static/css/2.03fe6687.chunk.css"
+  },
+  {
+    "revision": "a640c69c5afda01b2c6a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "03f8f8fd96bd7d8861ab",
+    "url": "/static/js/2.b274ac2d.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/static/js/2.b274ac2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a640c69c5afda01b2c6a",
+    "url": "/static/js/main.4caaf679.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
