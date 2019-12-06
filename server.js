aa9f06abe679a895f18f1ab7c1b32d9cc3efc9f1
ruthtech <leeruths@gmail.com>
2019-12-06 14:47:03 -0500

    Fix bug not showing page

--- server.js
+++ server.js
@@ -4,26 +4,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 
-// console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-// app.listen(port, (req, res) => {  
-//   console.log( `server listening on port: ${port}`);
-// })
 
-app.use(express.static(path.join(__dirname, 'client/public')));
-
-// if(process.env.NODE_ENV === 'production') {  
-//   app.use(express.static(path.join(__dirname, 'client/public'))); 
-// } else {
-  // app.get('*', (req, res) => {    
-  //   res.sendfile(path.join(__dirname = 'client/public/index.html'));  
+//if(process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, 'client/build'))); 
+  // app.get('/', (req, res) => {    
+  //   res.sendFile(path.join(__dirname+'/client/build/index.html'));  
   // })
-// }
+//} else {
+  //app.use(express.static(path.join(__dirname, 'client/build'))); 
+//}
 
-app.get('/', (req, res) => {  
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
+ app.get('/', (req, res) => {  
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  })
 // create a GET route
 app.get('/express_backend', (req, res) => {
   axios.get("https://api.adviceslip.com/advice")
